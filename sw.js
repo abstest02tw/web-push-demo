f7a2bcee78b1ef45df8420918e78f6335ed4ae46
abstest02tw <abstest02tw@gmail.com>
2024-12-30 17:16:07 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    console.log('Push received:', data);
+
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: '/icon.png'
+    });
+});
+
+
+
